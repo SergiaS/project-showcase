0a1558dfdff8333328dd934eb482f543b3725eb7
sergias <sergias@ukr.net>
2022-07-28 17:09:08 +0300

    08-004 Вывод списка товаров

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const API_KEY = process.env.REACT_PROJECT_SHOWCASE_API_KEY;
+const API_KEY = process.env.REACT_APP_PROJECT_SHOWCASE_API_KEY;
 
-const API_URL = 'https://fortniteapi.io/v2/shop?lang=en';
+const API_URL = 'https://fortniteapi.io/v1/shop?lang=en';
 
 export {
     API_KEY,
